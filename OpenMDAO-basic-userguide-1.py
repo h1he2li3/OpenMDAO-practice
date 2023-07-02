223bae61f1cc75056dec704d9d7805fee80ccf0a
Yashwanth Pochareddy <ykupo@pm.me>
2023-07-02 11:34:43 +0200

    more learning

---
+++ OpenMDAO-basic-userguide-1.py
@@ -0,0 +1,54 @@
+import openmdao.api as om
+
+# ExplicitComponent is contained in the openmdao.api module. This class is accessed through 'om.ExplicitComponent'.
+class Paraboloid(om.ExplicitComponent):
+    """
+    Evaluates the equation f(x,y) = (x-3)^2 + xy + (y+4)^2 - 3.
+    """
+
+    def setup(self):
+        self.add_input('x', val=0.0)
+        self.add_input('y', val=0.0)
+
+        self.add_output('f_xy', val=0.0)
+
+    def setup_partials(self):
+        # Finite difference all partials.
+        self.declare_partials('*', '*', method='fd')
+
+    def compute(self, inputs, outputs):
+        """
+        f(x,y) = (x-3)^2 + xy + (y+4)^2 - 3
+
+        Minimum at: x = 6.6667; y = -7.3333
+        """
+        x = inputs['x']
+        y = inputs['y']
+
+        outputs['f_xy'] = (x - 3.0)**2 + x * y + (y + 4.0)**2 - 3.0
+
+
+if __name__ == "__main__":
+
+    model = om.Group()
+    # parab_comp is an instance of the Paraboloid class
+    model.add_subsystem('parab_comp', Paraboloid())
+
+    # model Group with compoents, is passed to the constructor of the Problem class.
+    prob = om.Problem(model)
+    # call the setup() method on the problem.
+    prob.setup()
+
+    prob.set_val('parab_comp.x', 3.0)
+    prob.set_val('parab_comp.y', -4.0)
+
+    # to perform the computations
+    prob.run_model()
+    print(prob['parab_comp.f_xy'])
+
+    # Trying twice with different input values
+    prob.set_val('parab_comp.x', 5.0)
+    prob.set_val('parab_comp.y', -2.0)
+
+    prob.run_model()
+    print(prob.get_val('parab_comp.f_xy'))
